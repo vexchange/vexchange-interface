71040e7f47cce2f9d56d608dabe5a07ed00cb692
Kenneth Ashley <ken.u.diggitt@gmail.com>
2021-07-05 21:22:59 -0400

    Add analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
+import { isMobile } from 'react-device-detect'
 import { Web3ReactProvider, createWeb3ReactRoot } from './context'
 import Connex from '@vechain/connex'
 import { Provider } from 'react-redux'
@@ -23,6 +25,23 @@ function getLibrary() {
   })
 
   return connex
+}
+
+const GOOGLE_ANALYTICS_ID: string | undefined = process.env.REACT_APP_GOOGLE_ANALYTICS_ID
+
+if (typeof GOOGLE_ANALYTICS_ID === 'string') {
+  ReactGA.initialize(GOOGLE_ANALYTICS_ID, {
+    gaOptions: {
+      storage: 'none',
+      storeGac: false
+    }
+  })
+  // ReactGA.set({
+  //   anonymizeIp: true,
+  //   customBrowserType: !isMobile ? 'desktop' : 'web3' in window || 'ethereum' in window ? 'mobileWeb3' : 'mobileRegular'
+  // })
+} else {
+  ReactGA.initialize('test', { testMode: true, debug: true })
 }
 
 function Updaters() {
