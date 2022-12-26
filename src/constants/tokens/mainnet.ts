50fab46a5052b15b72c0d1257e095945a675173a
Kenneth Ashley <ken.u.diggitt@gmail.com>
2022-12-26 23:13:00 +0000

    add gold token

--- src/constants/tokens/mainnet.ts
+++ src/constants/tokens/mainnet.ts
@@ -22,5 +22,6 @@ export default [
   new Token(ChainId.MAINNET, '0x107a0b0faeb58c1fdef97f37f50e319833ad1b94', 18, 'DRAGON', 'Dragon Coin'),
   new Token(ChainId.MAINNET, '0x23368c20c16f64ecbb30164a08666867be22f216', 18, 'VSEA', 'VeSea Token'),
   new Token(ChainId.MAINNET, '0x34109fc2a649965eecd953d31802c67dcc183d57', 18, 'UNION', 'UNION'),
-  new Token(ChainId.MAINNET, '0xf01069227b814f425BAD4ba70ca30580F2297AE8', 18, 'BANANA', 'Banana coin')
+  new Token(ChainId.MAINNET, '0xf01069227b814f425BAD4ba70ca30580F2297AE8', 18, 'BANANA', 'Banana coin'),
+  new Token(ChainId.MAINNET, '0xFF3BC357600885aAA97506ea6e24fB21abA88fbD', 18, 'GOLD', 'GOLD Coin')
 ]
