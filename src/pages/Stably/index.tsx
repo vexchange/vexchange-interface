fb269c4be0c60e2cbdfe72d72479815424258c08
Kenneth Ashley <ken.u.diggitt@gmail.com>
2022-12-30 23:56:19 +0000

    update iframe to incluse account

--- src/pages/Stably/index.tsx
+++ src/pages/Stably/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { useWeb3React } from '../../hooks'
+
 export const Iframe = styled.iframe`
   position: relative;
   width: 100%;
@@ -9,10 +11,25 @@ export const Iframe = styled.iframe`
   border-radius: 0 0 26px 26px;
 `
 
+const Wrapper = styled.div`
+  text-align: center;
+  padding: 20px;
+`
+
 export default function Stably() {
+  const { account } = useWeb3React()
+
+  if (!account) {
+    return (
+      <Wrapper>
+        <div>Please sign in to continue</div>
+      </Wrapper>
+    )
+  }
+
   return (
     <div>
-      <Iframe src="https://ramp.stably.io/?address=hx777535db1b4039c837580e74aac35d0bbaaa7b4c&amount=42.42&network=icon&asset=USDS&filter=true"></Iframe>
+      <Iframe src={`https://ramp.stably.io/?address=${account}&network=vechain&asset=veusd&filter=true`}></Iframe>
     </div>
   )
 }
